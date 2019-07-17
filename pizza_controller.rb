b39ca644284120343004f54c594a244a07d5639d
= <evebramley@me.com>
2019-07-17 15:15:27 +0100

    lab kinda done

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -15,15 +15,40 @@ get '/pizza-orders/new' do
   erb(:new)
 end
 
-#SHOW
-get '/pizza-orders/:id' do
-  @order = PizzaOrder.find(params[:id])
-  erb(:show)
-end
-
 #CREATE
 post '/pizza-orders' do
   @order = PizzaOrder.new(params)
   @order.save()
   erb(:create)
+end
+
+#DESTROY
+# get '/pizza-orders/:id/delete' do
+#   @order = PizzaOrder.find(params[:id])
+#   erb(:delete)
+# end
+
+post '/pizza-orders/:id/delete' do
+  @order = PizzaOrder.find(params[:id])
+  @order.delete
+  redirect('/pizza-orders')
+end
+
+#EDIT
+get '/pizza-orders/:id/edit' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:edit)
+end
+
+#UPDATE
+post '/pizza-orders/:id' do
+  @order = PizzaOrder.new(params)
+  @order.update
+  erb(:update)
+end
+
+#SHOW
+get '/pizza-orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
 end
